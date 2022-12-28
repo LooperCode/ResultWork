2291d9ad33e26e39f695ba1b0889dd6f9a0867f0
Denis <Savchenkodkz@gmail.com>
2022-12-29 02:25:56 +0600

    добавил заполнение массива

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] FillArray (string []insert, int n)
+{
+    
+    for (int i = 0; i < insert.Length; i++)
+    {
+        Console.Write($"Введите значение элемента {i}: ");
+        insert[i] = Convert.ToString(Console.ReadLine());
+    }
+    return insert;
+}
+
+
+
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] insert = new string[n];
+FillArray(insert, n);
+
+
+
+
+
