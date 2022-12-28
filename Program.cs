36201020318798574eda57298c6a31a821bbcd24
Denis <Savchenkodkz@gmail.com>
2022-12-29 02:28:07 +0600

    добавил проверку длины итогового массива

--- Program.cs
+++ Program.cs
@@ -9,12 +9,27 @@
     return insert;
 }
 
+int CheckLength (string []insert, int count)
+{
+    
+    for (int i = 0; i < insert.Length; i++)
+    {
+        
+        if (insert[i].Length <= 3)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
 
 
 Console.Write("Введите размер массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 string[] insert = new string[n];
 FillArray(insert, n);
+int count = 0;
+string [] resultArr = new string [CheckLength(insert, count)];
 
 
 
